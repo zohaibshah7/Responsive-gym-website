902edbbfb3f9b542ccd12c594259a1a89970271b
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2024-03-01 22:22:26 +0500

    Logos section & header animation with js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,13 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
